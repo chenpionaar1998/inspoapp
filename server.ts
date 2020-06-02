5217077dd4a6f5fcfe2707cc918ef85e08ab387a
twc10 <twc10@sfu.ca>
2020-06-02 23:43:54 +0800

    fix for ts error in heroku deployment

--- server.ts
+++ server.ts
@@ -31,6 +31,16 @@ if (dev === "production") {
     });
 }
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    }
+    else {
+        next()
+
+    }
+})
+
 app.post("/api/signUp", async (req, res): Promise<void> => {
     const result = signUp(req.body);
     result.then(ress => {

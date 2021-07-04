f5f786de590dd947f1dd62cc626c6565e5b0d6ba
twc10 <dean_chiu@sfu.ca>
2021-07-04 09:56:57 +0800

    pg connection issue

--- server/database/api/findUser.ts
+++ server/database/api/findUser.ts
@@ -5,6 +5,7 @@ import pool from '../database';
 import { AccountInfoType } from '../../../client/src/Components/Account/Signup/SignUpForm';
 
 export async function findUser (email: string): Promise<boolean> {
+    pool.connect();
     const result = await pool.query("SELECT EXISTS(SELECT email FROM users WHERE email=$1);", [email]);
 
     console.log("RESULT: ", result.rows[0].exists)
@@ -13,6 +14,7 @@ export async function findUser (email: string): Promise<boolean> {
 }
 
 export async function getUser (email: string): Promise<AccountInfoType> {
+    pool.connect();
     const user = await pool.query("SELECT * FROM users WHERE email=$1;", [email]);
     
     return user.rows[0] as unknown as AccountInfoType;

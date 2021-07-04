57a07a6e3861c5cb48a800b7cb4886533ee3a581
twc10 <dean_chiu@sfu.ca>
2021-07-04 09:24:51 +0800

    remove cors

--- server.ts
+++ server.ts
@@ -22,11 +22,11 @@ const dev = app.get('env');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 if (dev === "production") {
     app.disable("x-powered-by");
